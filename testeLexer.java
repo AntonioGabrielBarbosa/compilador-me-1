8a9be8dc212bd9cc785ddab2d2f03cac9cb571d5
AntonioGabrielBarbosa <antonio.gabrielbarbosa2003@hotmail.com>
2025-03-26 16:53:46 -0300

    testes para analisador léxico.

--- testeLexer.java
+++ testeLexer.java
@@ -2,12 +2,37 @@ import java.util.List;
 
 public class testeLexer {
     public static void main(String[] args) {
-        String code = "if ( hero + 10 ) { move_up; attack; }";
-        Lexer lexer = new Lexer(code);
-        List<Token> tokens = lexer.tokenize();
-        
-        for (Token token : tokens) {
-            System.out.println(token);
+
+        String[] testCases = {
+
+            "if ( hero + 10 ) { move_up; attack; }",
+            "while ( enemy - 5 ) { defend; }",
+            "for ( hero; enemy; treasure ) { move_left; }",
+            "if ( treasure ) { jump; }",
+            "hero + enemy - trap;",
+            
+
+            "move_right!", 
+            "123abc", 
+            "if ( 5hero ) { attack; }", 
+            "move_up ;;; attack;", 
+            "run_away;", 
+        };
+
+        for (String code : testCases) {
+            System.out.println("\n--- Testando código ---");
+            System.out.println(code);
+
+
+            Lexer lexer = new Lexer(code);
+            List<Token> tokens = lexer.tokenize();
+
+            System.out.println("\nTokens gerados:");
+            for (Token token : tokens) {
+                System.out.println(token);
+            }
+
+            System.out.println("\n========================\n");
         }
     }
 }
